bf5f6ba4b47258ac24352bd43dbee981d993b791
Joftal <42807405+Joftal@users.noreply.github.com>
2026-02-07 20:55:47 +0800

    build

--- scripts/build_audio_separator.py
+++ scripts/build_audio_separator.py
@@ -137,7 +137,7 @@ coll = COLLECT(
 ENTRY_SCRIPT = '''#!/usr/bin/env python3
 """audio-separator entry point"""
 import sys
-from audio_separator.separator import main
+from audio_separator.utils.cli import main
 
 if __name__ == '__main__':
     sys.exit(main())
